84d142366f38ee348063c8d52e9ebfa1a49c2821
황지우 <skyro13@naver.com>
2022-03-02 16:40:28 +0900

    test2

--- src/main/java/com/mydeco/controller/TestController.java
+++ src/main/java/com/mydeco/controller/TestController.java
@@ -11,4 +11,22 @@ public class TestController {
 		System.out.println("test");
 		return "user/modifyForm";
 	}
+	
+	@RequestMapping("/main")
+	public String main() {
+		System.out.println("main");
+		return "main/main";
+	}
+	
+	@RequestMapping("/mtest")
+	public String mtest() {
+		System.out.println("mood test");
+		return "mood/test";
+	}
+	
+	@RequestMapping("/daily")
+	public String daily() {
+		System.out.println("daily");
+		return "mood/daily";
+	}
 }
