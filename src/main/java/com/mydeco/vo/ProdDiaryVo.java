daaf8d01143552243c48219c052068bf0729fc51
chaerin <77728068+Boram8878@users.noreply.github.com>
2022-03-23 16:18:01 +0900

    prodDiaryVo 수정

--- src/main/java/com/mydeco/vo/ProdDiaryVo.java
+++ src/main/java/com/mydeco/vo/ProdDiaryVo.java
@@ -5,43 +5,74 @@ public class ProdDiaryVo {
 	private int prodDiaryNo;
 	private int prodNo;
 	private int diaryNo;
+	private int count;
 
 	public ProdDiaryVo() {
 		super();
 	}
 
-	public ProdDiaryVo(int prodNo, int diaryNo) {		
+	
+	public ProdDiaryVo(int prodNo, int diaryNo) {
+		super();
 		this.prodNo = prodNo;
 		this.diaryNo = diaryNo;
 	}
+
+
+	public ProdDiaryVo(int prodDiaryNo, int prodNo, int diaryNo, int count) {
+		super();
+		this.prodDiaryNo = prodDiaryNo;
+		this.prodNo = prodNo;
+		this.diaryNo = diaryNo;
+		this.count = count;
+	}
+
 
 	public int getProdDiaryNo() {
 		return prodDiaryNo;
 	}
 
+
 	public void setProdDiaryNo(int prodDiaryNo) {
 		this.prodDiaryNo = prodDiaryNo;
 	}
+
 
 	public int getProdNo() {
 		return prodNo;
 	}
 
+
 	public void setProdNo(int prodNo) {
 		this.prodNo = prodNo;
 	}
+
 
 	public int getDiaryNo() {
 		return diaryNo;
 	}
 
+
 	public void setDiaryNo(int diaryNo) {
 		this.diaryNo = diaryNo;
 	}
 
+
+	public int getCount() {
+		return count;
+	}
+
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+
 	@Override
 	public String toString() {
-		return "ProdDiaryVo [prodDiaryNo=" + prodDiaryNo + ", prodNo=" + prodNo + ", diaryNo=" + diaryNo + "]";
+		return "ProdDiaryVo [prodDiaryNo=" + prodDiaryNo + ", prodNo=" + prodNo + ", diaryNo=" + diaryNo + ", count="
+				+ count + "]";
 	}
+
 
 }
